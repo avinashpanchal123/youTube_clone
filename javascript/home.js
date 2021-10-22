3c5c41726e8332cdf0d90e81b65008f4bede7bf9
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2021-10-22 19:16:17 +0530

    homePage completed

--- javascript/home.js
+++ javascript/home.js
@@ -1,52 +1,131 @@
-//AIzaSyCXUjXRuJZ7uYBlGjAtuqFYUQz5ULz-CdY
+//GET https://youtube.googleapis.com/youtube/v3/search?location=India&q=popular%20videos&type=video&key=[YOUR_API_KEY] HTTP/1.1
 
-//https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=popular&type=video&key=[YOUR_API_KEY] HTTP/1.1
-
-
-
-// let getData = async () => {
-
-//     try {
-//         let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=popular&type=video&key=AIzaSyCXUjXRuJZ7uYBlGjAtuqFYUQz5ULz-CdY`)
-
-//         let data = await res.json()
-
-//         console.log(data);
-//          appendVideos(data.items)
-
-//     }
-
-//     catch(err){
-//         console.log(err);
-//     }
-
-// }
-
-// getData()
+//https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=${query}&type=video&key
 
 
 
-// function appendVideos(videos) {
 
-//     videos_container.innerHTML = null;
+let getData = async () => {
 
-//     videos.forEach(({ id: { videoId } }) => {
+    
+    try {
+        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=popular%20videos&type=video&key=AIzaSyCXUjXRuJZ7uYBlGjAtuqFYUQz5ULz-CdY`)
+
+        let data = await res.json()
+
+        console.log(data.items.videoId);
+         appendVideos(data.items)
+
+    }
+
+    catch(err){
+        console.log(err);
+    }
+}
+
+getData()
+
+let videos_container = document.getElementById("videos_container");
 
 
 
-//       let div = document.createElement("div");
 
-//       console.log(videoId);
+function appendVideos(videos) {
 
-//       div.innerHTML = `<iframe width="150" height="150" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    videos_container.innerHTML = null;
 
-//       div.style.marginTop = "20px"
-
-//       videos_container.append(div)
-
-//     });
+    videos.forEach(({ id: { videoId } }) => {
 
 
-// }
 
+      let div = document.createElement("div");
+
+      console.log(videoId);
+
+      div.innerHTML = `<iframe width="200" height="150" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+      div.style.marginTop = "20px"
+
+      videos_container.append(div)
+
+    });
+
+
+}
+
+
+ // going to sign in page 
+
+ let login_btn = document.getElementById("login_btn");
+
+ login_btn.onclick = () =>{
+     goToSignInPage()
+ }
+
+
+//  function main(){
+//      let query = document.getElementById("query").value;
+     
+//      searchVideo(query)
+
+
+//  }
+
+// fetching the data according to the input
+
+searchVideos = async()=>{
+
+    let query = document.getElementById("query").value;
+
+    try{
+        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q=${query}&type=video&key=AIzaSyCXUjXRuJZ7uYBlGjAtuqFYUQz5ULz-CdY`)
+
+        let data = await res.json();
+
+        console.log(data);
+
+        appendVideos(data.items)
+    }
+
+    catch(err){
+          console.log(err);
+    }
+
+ }
+ 
+
+//   including debouncing in the search box
+ let timer;
+
+ let video_list_container = document.querySelector(".video_list_container");
+
+
+ debounce = (func,delay)=>{
+
+     video_list_container.style.display = "block"
+
+    clearTimeout(timer)
+
+    timer = setTimeout( () =>{
+        func()
+    },delay)
+    
+ }
+
+ //adding event listener to the body for 
+
+ let body = document.querySelector("body");
+
+
+body.onclick = ()=>{
+     let timeout = setTimeout(()=>{
+        gayab()
+     },1000)
+ }
+
+ gayab = ()=>{
+    video_list_container.style.display = "none"
+ }
+
+ 
 
